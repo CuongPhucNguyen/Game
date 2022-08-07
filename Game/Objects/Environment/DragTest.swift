bb26ea2ad42339a6e8f64f90f60e3af473767eb8
Cuong <s3881006@rmit.edu.vn>
2022-08-07 12:49:16 +0700

    implemented basic shooting mechanic but the trajectory is still using the mouse location instead of the center of the circle. Must fix

--- Game/Objects/Environment/DragTest.swift
+++ Game/Objects/Environment/DragTest.swift
@@ -14,44 +14,57 @@ struct DragTest: View {
     // how far the circle has been dragged
     @State private var offset = CGSize.zero
     @State var accumulated: CGSize = CGSize.zero
+    @State var prevPos: CGSize = CGSize.zero
 
     // whether it is currently being dragged or not
     @State private var isDragging = false
 
     var body: some View {
-        // a drag gesture that updates offset and isDragging as it moves around
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                offset = CGSize(width: value.translation.width + accumulated.width, height: value.translation.height + accumulated.height)
-                
-            }
-            .onEnded { value in
-                withAnimation {
-                    isDragging = false
+        Group{
+            // a drag gesture that updates offset and isDragging as it moves around
+            let dragGesture = DragGesture()
+                .onChanged { value in
+                    prevPos = accumulated
                     offset = CGSize(width: value.translation.width + accumulated.width, height: value.translation.height + accumulated.height)
-                    accumulated = offset
                     
                 }
-            }
-
-        // a long press gesture that enables isDragging
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    isDragging = true
+                .onEnded { value in
+                    withAnimation {
+                        isDragging = false
+                        offset = CGSize(width:prevPos.width - ( value.translation.width*1.5 + accumulated.width), height: prevPos.height - (value.translation.height*1.5 + accumulated.height))
+                        accumulated = offset
+                        
+                    }
                 }
+            // a combined gesture that forces the user to long press then drag
+    //        let combined = pressGesture.sequenced(before: dragGesture)
+
+            // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
+            ZStack{
+                Circle()
+                    .fill(.blue)
+                    .frame(width: 64, height: 64)
+                    .scaleEffect(isDragging ? 1.5 : 1)
+                    .offset(prevPos)
+                Circle()
+                    .fill(.red)
+                    .frame(width: 64, height: 64)
+                    .scaleEffect(isDragging ? 1.5 : 1)
+                    .offset(offset)
+                    .gesture(dragGesture)
+                
             }
-
-        // a combined gesture that forces the user to long press then drag
-//        let combined = pressGesture.sequenced(before: dragGesture)
-
-        // a 64x64 circle that scales up when it's dragged, sets its offset to whatever we had back from the drag gesture, and uses our combined gesture
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(dragGesture)
+            
+        }
+//        .onChange(of: inertia)
+//            { newChange in
+//                withAnimation{
+//                    offset = CGSize(width: prevPos.width-accumulated.width*2, height:prevPos.height-accumulated.height*2)
+//                    accumulated = offset
+//                    inertia = false
+//                }
+//            }
+            
     }
 }
 
