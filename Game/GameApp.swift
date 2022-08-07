228036e21c7aa5bb06717c09219eed8cba4222dc
Cuong <s3881006@rmit.edu.vn>
2022-08-07 10:08:59 +0700

    Initial Commit

---
+++ Game/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  Game
+//
+//  Created by Cuong Nguyen Phuc on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
