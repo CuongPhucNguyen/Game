8260315b1ef270cfb762696799dddd6005c754a5
Cuong <s3881006@rmit.edu.vn>
2022-08-28 23:58:37 +0700

    asd

--- Game/Resources/DataSave/UsersData.swift
+++ Game/Resources/DataSave/UsersData.swift
@@ -9,8 +9,9 @@ class UsersData: Codable{
   var killBalls: KillBallHandler
   var easyMode: Bool
   var leaderboard: [PlayerData]
-    init(currentPosition: PlayerPosition, scores: Int, pointBalls: PointBallHandler, killBalls: KillBallHandler, easyMode: Bool, leaderboard: [PlayerData]){
-        self.currentPosition = currentPosition
+    init(currentPositionWidth: Double, currentPositionHeight: Double, scores: Int, pointBalls: PointBallHandler, killBalls: KillBallHandler, easyMode: Bool, leaderboard: [PlayerData]){
+        self.currentPositionWidth = currentPositionWidth
+        self.currentPositionHeight = currentPositionHeight
         self.scores = scores
         self.pointBalls = pointBalls
         self.killBalls  = killBalls
